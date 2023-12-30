f4285f52c138d1342813994ff45c13b4ab32580a
asya474 <asya474@gmail.com>
2023-12-31 03:25:28 +0700

    third commit

---
+++ tests/conftest.py
@@ -0,0 +1,100 @@
+import os
+import allure
+import allure_commons
+from appium import webdriver
+
+import utils.attach
+import pytest
+import config
+from appium.options.android import UiAutomator2Options
+from appium.options.ios import XCUITestOptions
+from selene import browser, support
+
+
+@pytest.fixture(scope='function')
+def android_mobile_management():
+    options = UiAutomator2Options().load_capabilities({
+        # Specify device and os_version for testing
+        "platformName": "android",
+        "platformVersion": "13.0",
+        "deviceName": "Samsung Galaxy S23 Ultra",
+
+        # Set URL of the application under test
+        "app": "bs://c1c52615799e5efca0852cc5e27e222eb04bb041",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "Android tests",
+            "buildName": "browserstack-wikipedia-build",
+            "sessionName": "BStack wikipedia_test",
+
+            # Set your access credentials
+            "userName": config.username,
+            "accessKey": config.access_key
+        }
+    })
+
+    browser.config.driver = webdriver.Remote(
+        config.remote_browser_url,
+        options=options
+    )
+    browser.config.timeout = 10.0
+
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext)
+
+    yield
+    utils.attach.attach_bstack_screenshot()
+    utils.attach.attach_bstack_page_source()
+
+    session_id = browser.driver.session_id
+
+    with allure.step('tear down app session'):
+        browser.quit()
+
+    utils.attach.attach_bstack_video(session_id)
+
+
+@pytest.fixture(scope='function')
+def ios_mobile_management():
+    options = XCUITestOptions().load_capabilities({
+        # Specify device and os_version for testing
+        "platformName": "ios",
+        "platformVersion": "16",
+        "deviceName": "iPhone 14",
+
+        # Set URL of the application under test
+        "app": "bs://444bd0308813ae0dc236f8cd461c02d3afa7901d",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "Ios tests",
+            "buildName": "browserstack-simple-app-build",
+            "sessionName": "BStack Simple app test",
+
+            # Set your access credentials
+            "userName": config.username,
+            "accessKey": config.access_key
+        }
+    })
+
+    browser.config.driver = webdriver.Remote(
+        config.remote_browser_url,
+        options=options
+    )
+    browser.config.timeout = 10.0
+
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext)
+
+    yield
+
+    utils.attach.attach_bstack_screenshot()
+    utils.attach.attach_bstack_page_source()
+
+    session_id = browser.driver.session_id
+
+    with allure.step('tear down app session'):
+        browser.quit()
+
+    utils.attach.attach_bstack_video(session_id)
---
+++ tests/test_android_wiki.py
@@ -0,0 +1,36 @@
+import pytest
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_search_wiki(android_mobile_management):
+    with step('Tap on the skip button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button')).click()
+
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia:id/search_src_text")).type(
+            'Appium')  # org.wikipedia.alpha:id/search_src_text
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+# com.android.chrome:id/title_url_container
+def test_open_some_article(android_mobile_management):
+    with step('Tap on the skip button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button')).click()
+
+    with step('Open wiki and search the article'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia:id/search_src_text")).type(
+            'Japan')  # android.widget.TextView
+        browser.element((AppiumBy.ID, "org.wikipedia:id/page_list_item_title")).click()
+
+    with step('Verify article found'):
+        browser.element((AppiumBy.XPATH,
+                         "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.view.ViewGroup/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[1]/android.view.View[1]")).should(
+            have.text('Japan'))
