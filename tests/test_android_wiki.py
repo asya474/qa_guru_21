ad55d03337aa096c42cb42b53e17d00c7917761d
asya474 <asya474@gmail.com>
2024-01-04 22:58:23 +0700

    bs app for android commit

--- tests/test_android_wiki.py
+++ tests/test_android_wiki.py
@@ -9,25 +9,25 @@ def test_search_wiki(android_mobile_management):
 
     with step('Type search'):
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_container")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia:id/search_src_text")).type(
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
             'Appium')  # org.wikipedia.alpha:id/search_src_text
 
     with step('Verify content found'):
-        results = browser.all((AppiumBy.ID, 'org.wikipedia:id/page_list_item_title'))
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
 
 
 # com.android.chrome:id/title_url_container
 def test_open_some_article(android_mobile_management):
-    with step('Tap on the skip button'):
-        browser.element((AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button')).click()
+    #with step('Tap on the skip button'):
+        #browser.element((AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button')).click()
 
     with step('Open wiki and search the article'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia:id/search_src_text")).type(
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_container")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
             'Japan')  # android.widget.TextView
-        browser.element((AppiumBy.ID, "org.wikipedia:id/page_list_item_title")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")).click()
 
     with step('Verify article found'):
         browser.element((AppiumBy.XPATH,
